7f855d01af17eb160756b434d4c62c6d5d6d81ad
maycout2@gmail.com <maycout2@gmail.com>
2017-04-04 11:57:10 -0300

    NewFlapEtapa1

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Estado estado { get;  private set; }
+
+    public GameObject obstaculo;
+    public float espera;
+    public float tempoDestruicao;
+
+    public static GameController instancia = null;
+
+    void Awake()
+    {
+        if (instancia == null)
+        {
+            instancia = this;
+        }
+        else if (instancia!= null)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    
+	void Start () {
+        estado = Estado.AguardoComecar;
+        StartCoroutine(GerarObstaculos());
+	}
+
+    IEnumerator GerarObstaculos()
+    {
+        while (GameController.instancia.estado == Estado.Jogando)
+        {
+            Vector3 pos = new Vector3(0f, Random.Range(-2.0f, 5.0f), 0f);
+            GameObject obj = Instantiate(obstaculo, pos, Quaternion.identity) as GameObject;
+            Destroy(obj, tempoDestruicao);
+            yield return new WaitForSeconds(espera);
+        }
+    }
+	
+	public void PlayerComecou()
+    {
+        estado = Estado.Jogando;
+        StartCoroutine(GerarObstaculos());
+    }
+
+    public void PlayerMorreu()
+    {
+        estado = Estado.GameOver;
+    }
+}
